1d308680813b33d8d6303defaba32266962a2473
rully <rully.saputra4@gmail.com>
2022-08-01 22:55:28 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import {Center, Text, VStack} from "@chakra-ui/react";
+
+const App = () => {
+  return (
+    <VStack justifyContent="center" h="100vh">
+        <Center>
+          <Text>Form Ritual in construction!</Text>
+        </Center>
+    </VStack>
+  )
+}
+
+export default App
