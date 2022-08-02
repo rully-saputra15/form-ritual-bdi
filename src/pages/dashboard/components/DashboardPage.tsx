3ba19d90e5d142f84d20fa8a246d959259c12468
rully <rully.saputra4@gmail.com>
2022-08-02 22:31:14 +0700

    refactor: instruction on dashboard page

--- src/pages/dashboard/components/DashboardPage.tsx
+++ src/pages/dashboard/components/DashboardPage.tsx
@@ -11,7 +11,7 @@ const DashboardPage: FC<DashboardPageProps> = () => {
     <VStack alignItems="flex-start" m={3} spacing={3}>
       <Heading fontSize="4xl">List Form</Heading>
       <Text>Berikut berisi form-form ritual</Text>
-      <Text>Anda hanya perlu <b>menekan salah satu item dibawah</b> dan akan <b>secara otomatis dicopy</b> pada smartphone Anda.</Text>
+      <Text>Anda hanya perlu <b>menekan salah satu item dibawah</b> dan link akan <b>secara otomatis dicopy</b> pada smartphone Anda.</Text>
       <Text mt={3}>Silahkan kirim link tersebut kepada umat yang membutuhkan.</Text>
       <CardOnlyTitle title="Gojukai" />
       <CardOnlyTitle title="Pemberkahan Nikah" />
